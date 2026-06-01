9c9d55a526fd04229e0c68a4e4dd9af8969a23f6
Anderson Moraes <hjhjsdsscxqw@gmail.com>
2026-05-31 22:19:48 -0300

    Inclusão de catálogo

---
+++ cypress/e2e/catalogo.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Catálogo de Livros', () => {
+
+    beforeEach(() => {
+        cy.visit('catalog.html')
+    });
+    
+    it('Deve clicar no botão Adicionar à cesta', () => {
+        
+        
+    });
+});
